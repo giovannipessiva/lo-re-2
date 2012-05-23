5910f64f077a817502c90331d1fe383c1343db0b
Giovanni Pessiva <giovanni.pessiva@gmail.com>
2012-05-23 12:21:32 +0200

    source code moved to /src

--- README.md
+++
@@ -1,4 +0,0 @@
-lo-re-2
-=======
-
-Lo Re 2 is a C++ command-line implementation of the card game "Lo re"
--- test/t6_game.cpp
+++
@@ -1,8 +0,0 @@
-#include "../Game.h"
-#include <iostream>
-using namespace std;
-
-void test_game()
-{
-
-}
